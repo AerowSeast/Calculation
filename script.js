5636e64369f14b327b988eb444113005cd7ee74b
Josh <Boemoken84167@gmail.com>
2022-07-17 11:58:40 +0800

    insert calculation using

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.querySelector("#display");
+let buttons = Array.from(document.querySelectorAll("table tr td"));
+buttons.forEach(item => { 
+    item.addEventListener("click", e => { 
+        if (item.innerText == "C") {
+            display.innerText = "";
+        } else if (item.innerText == "=") {
+            display.innerText = eval(display.innerText);
+         }
+         else { 
+            display.innerText += item.innerText;
+        }
+    })
+})
+
+
+
